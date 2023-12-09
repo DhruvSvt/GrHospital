c2742427b165451377a1b9e2226c182a048725f9
DhruvSvt <dhruvsvt08@gmail.com>
2023-12-09 13:32:26 +0530

    Doctors

---
+++ database/migrations/2023_12_09_072121_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('specialty')->nullable();
+            $table->string('desc')->nullable();
+            $table->smallInteger('status')->default(0)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
