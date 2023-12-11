f9ec02c3d1f6ef0275d99c239913b67087141c68
DhruvSvt <dhruvsvt08@gmail.com>
2023-12-11 13:38:47 +0530

    Gallery

---
+++ database/migrations/2023_12_09_114133_create_galleries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->string('ordering')->nullable();
+            $table->string('image')->nullable();
+            $table->smallInteger('status')->default(0)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+};
